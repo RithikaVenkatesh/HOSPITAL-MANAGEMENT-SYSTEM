1310ef2cf26827a53c7baa77d3446cd6f758c443
RithikaVenkatesh <145837123+RithikaVenkatesh@users.noreply.github.com>
2024-10-17 21:51:25 +0530

    index1.php

--- index1.php
+++ index1.php
@@ -11,13 +11,7 @@ include("header.php");
     <!-- Bootstrap CSS -->
     <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-    <link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-
-    
-
-
-    
   </head>
   <style type="text/css">
     #inputbtn:hover{cursor:pointer;}
@@ -56,31 +50,22 @@ include("header.php");
     </div>
   </nav>
 
-
-
     <div class="container-fluid" style="margin-top:60px;margin-bottom:60px;color:#34495E;">
       <div class="row">
-
-        
-
          <div class="col-md-7" style="padding-left: 180px; ">
                  <div style="-webkit-animation: mover 2s infinite alternate;
     animation: mover 1s infinite alternate;">
           </div>
-
-      <div style="color: white;">
-            
+      <div style="color: white;">   
           </div>
-
          </div>
-
          <div class="col-md-4" style="margin-top: 5%;right: 8%">
           <div class="card" style="font-family: 'IBM Plex Sans', sans-serif;">
             <div class="card-body">
                 <i class="fa fa-hospital-o fa-3x" aria-hidden="true" style="color:#0062cc"></i>
                 <br>
-                <center>
-              <h3 style="margin-top: 10%">Patient Login</h3><br>
+                <centre>
+              <h3 style="margin-top: 10%"><centre>Patient Login</centre></h3><br>
               <form class="form-group" method="POST" action="func.php">
                 <div class="row" style="margin-top: 10%">
                   <div class="col-md-4"><label>Email-ID: </label></div>
@@ -90,28 +75,18 @@ include("header.php");
                 </div>
                 <div class="row">
                  <div class="col-md-4"  style="padding-left: 160px;margin-top: 10%">
-                    <center><input type="submit" id="inputbtn" name="patsub" value="Login" class="btn btn-primary"></center></div>           
+                    <centre><input type="submit" id="inputbtn" name="patsub" value="Login" class="btn btn-primary"></centre></div>           
                  <!--  <div class="col-md-8" style="margin-top: 10%">
                     <a href="index.php" class="btn btn-primary">Back</a></div> -->
                 </div>
               </form>
-            </center>
+            </centre>
             </div>
           </div>
-        </div>
-
-
-      </div>
+        </div>   
     </div>
-
-
-
-
-
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
   </body>
 </html>
